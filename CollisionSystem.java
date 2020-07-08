c998b55893d1b6be5367df6001ff3a0561e1a11e
abeRC <aeciobeltrao@usp.br>
2020-07-08 18:49:56 -0300

    α

---
+++ CollisionSystem.java
@@ -0,0 +1,5 @@
+public class CollisionSystem {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Particle.java
@@ -0,0 +1,5 @@
+public class Particle {
+    public static void main(String[] args) {
+
+    }
+}
