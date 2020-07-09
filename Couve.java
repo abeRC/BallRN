b2e30d8e58946e272b3b21d155d30b77043d8dea
abeRC <aeciobeltrao@usp.br>
2020-07-09 10:21:40 -0300

    Particle.java ok, but missing Couve.

---
+++ Couve.java
@@ -0,0 +1,15 @@
+public class Couve {
+    public static double dotProduct (double[] arr1, double[] arr2) {
+        return 0;
+    }
+
+    public static void scaledIncrement (double[] arr, double s, double[] b) {
+        //faz arr = arr + s*b, para todas as posicoes dos vetores
+    }
+
+    public static double[] scale (double s, double[] arr) {
+        double[] ret = null;
+        //faz ret = s*arr, para todas as posições do vetor, e depois retorna ret
+        return ret;
+    }
+}
