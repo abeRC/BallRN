f3991fd2998f6d3fa1fdd6993f023de1151ab629
abeRC <aeciobeltrao@usp.br>
2020-07-10 05:01:11 -0300

    formatacao

--- Couve.java
+++ Couve.java
@@ -1,16 +1,13 @@
-
 public class Couve {
     private static double dotProduct(double[]arr1,double[]arr2)
-{
-   double produtoEscalar=0;
+    {
+        double produtoEscalar=0;
 
-   for(int i=0; i<arr1.length; i++)
-        produtoEscalar += arr1[i] * arr2[i];
+        for(int i=0; i<arr1.length; i++)
+            produtoEscalar += arr1[i] * arr2[i];
    
-    return produtoEscalar;
-
-
-}
+        return produtoEscalar;
+    }
 
     public static void scaledIncrement (double[] arr, double s, double[] b) {
         //faz arr = arr + s*b, para todas as posicoes dos vetores
@@ -21,11 +18,11 @@ public class Couve {
         //faz ret = s*arr, para todas as posições do vetor, e depois retorna ret
         return ret;
     }
-public static void main(String[] args){
-   double[]a={0,3,5,-1};
-   double[]b={3,0,5,0.95};
-   System.out.println(dotProduct(a,b));
-}
-}
 
+    public static void main(String[] args){
+        double[]a={0,3,5,-1};
+        double[]b={3,0,5,0.95};
+        System.out.println(dotProduct(a,b));
+    }
+}
 
