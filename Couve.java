b1d8d93575b81b34e2371d7729d7e1f5c7b21898
Lia Simões <lians@usp.br>
2020-07-12 02:51:34 -0300

    implementado o scaledIncrement

--- Couve.java
+++ Couve.java
@@ -4,15 +4,18 @@ public static void scaledIncrement(double[]a,double[]b,double s)
 {
    for(int i=0; i<a.length; i++)
         a[i]=a[i]+s*b[i];
-   		}
+}
 public static void main(String[] args){
-   double[]a={0,3,5,-1};
-   double[]b={3,0,5,0.95};
+   double[]a={0,3,5,-1, 7, 0,3,2,1, 5, 6, 7, 0, 1, 2,3,5,-1, 7, 0,3,2,1, 5, 6, 7, 0, 1, 2, 1};
+   double[]b={3,0,5,0.95, 7,5,3,2,1, 5, 6, 7, 0, 1, 2,3,0,5,0.95, 7,5,3,2,1, 5, 6, 7, 0, 1, 2};
    double c=3;
    System.out.println(Arrays.toString(a));
    scaledIncrement(a,b,c);
    System.out.println("novo a:"+Arrays.toString(a)); 
    System.out.println(dotProduct(a,b));
+   System.out.println(Arrays.toString(scale(c,a)));
+   Double[] arr = {4.2, 2.3333, 1., 9.};
+   printArray(arr);
 }
 private static double dotProduct(double[]arr1,double[]arr2)
 {
@@ -22,5 +25,20 @@ private static double dotProduct(double[]arr1,double[]arr2)
         produtoEscalar += arr1[i] * arr2[i];
    
     return produtoEscalar;
+}
+private static double[]scale(double s,double[]arr)
+{
+   double[]ret=null;
+   ret=new double[arr.length];
+   for(int i=0; i<arr.length; i++)
+        ret[i]=s*arr[i];
+      return ret;
+}
+private static void printArray(Object[]arr)
+{
+   double[]ret=null;
+   ret=new double[arr.length];
+   for(int i=0; i<arr.length; i++)
+        System.out.println("posicao"+i+": "+arr[i]);
 }
 }
