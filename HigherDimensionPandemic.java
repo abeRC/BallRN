c2332574c18122987ec01b689ccec647773a3bc8
abeRC <aeciobeltrao@usp.br>
2020-07-11 23:42:35 -0300

    started work on HigherDimensionPandemic; missing integration with CollisionSystem

--- HigherDimensionPandemic.java
+++ HigherDimensionPandemic.java
@@ -1,4 +1,121 @@
-package PACKAGE_NAME;
+import com.jme3.app.SimpleApplication;
+import com.jme3.asset.plugins.FileLocator;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Sphere;
+import com.jme3.system.AppSettings;
+import com.jme3.texture.Texture;
 
-public class HigherDimensionPandemic {
+/**
+ * Simulates a higher-dimension pandemic and displays a 3D (3 possibly random dimensions
+ * out of N) view of it with jMonkeyEngine.
+ * */
+public class HigherDimensionPandemic extends SimpleApplication {
+    /**TODO 1. pick 3 dimensions*/
+    /*
+     * The jME game loop has 3 phases:
+     *      Init: The simpleInitApp() method is executed only once, right at the beginning;
+     *      Update: The simpleUpdate() method runs repeatedly, during the game.
+     *      Render: After every update, the jMonkeyEngine automatically redraws (renders)
+     *              the screen for you.
+     * To make a jME game, we extend SimpleApplication and override the methods that handle
+     * the init and update phases.
+     *
+     * The game scene is made out of Spatial objects, which can be divided into 2 subclasses:
+     *      Geometry, which is a mesh (shape) with a material;
+     *      Node, which is an invisible handler for grouping Geometry and Node objects together.
+     * Only Geometry objects attached to the rootNode are displayed.
+     * */
+
+    public final int[] chosenDimensions = {0, 1, 2};
+    public final float wallDistance = 100;
+    private int BEINGCOUNT = 0;
+    private Being ball;
+
+    public static void main (String[] args){
+        HigherDimensionPandemic app = new HigherDimensionPandemic();
+        AppSettings as = new AppSettings(true); //default settings
+        as.put("Title", "HigherDimensionPandemic"); as.put("VSync", true); as.put("Width", 1280); as.put("Height", 720);
+        app.setShowSettings(false); // Uncomment to skip the initial settings popup
+        app.setSettings(as);
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        //assetManager.registerLocator("Pictures", FileLocator.class);
+        flyCam.setMoveSpeed(7); // Make the camera more bearable.
+        Particle p = new Particle(3);
+        ball = new Being(p);
+        makewalls();
+    }
+
+    /**Main event loop.
+     * @param tpf time per frame*/
+    @Override
+    public void simpleUpdate(float tpf) {
+        Vector3f curpos = ball.g.getLocalTranslation();
+        ball.g.setLocalTranslation(curpos.add(0, 0, tpf));
+    }
+
+    /**Auxiliary function to make the (visual) walls.*/
+    private void makewalls () {
+        float halfPi = (float)Math.PI/2;
+        Vector3f[] positions = {
+                new Vector3f(0, 0, wallDistance),
+                new Vector3f(0, 0, -wallDistance),
+                new Vector3f(wallDistance, 0, 0),
+                new Vector3f(-wallDistance, 0, 0),
+                new Vector3f(0, wallDistance, 0),
+                new Vector3f(0, -wallDistance, 0),
+        };
+
+        Texture space = assetManager.loadTexture("assets/Pictures/Cosmic Winter Wonderland.jpg");
+        for (int i = 0; i < 6; i++) {
+            Material mat = new Material(assetManager,
+                    "Common/MatDefs/Misc/Unshaded.j3md"); //default material
+            mat.setTexture("ColorMap", space);
+            Box mesh = new Box(200, 3, 200);
+
+            Geometry g = new Geometry("wall" + i, mesh);
+            g.setMaterial(mat);
+            g.setLocalTranslation(positions[i]);
+
+            /*0 <= i < 2: xAngle = halfPi, yAngle = 0, zAngle = 0
+            * 2 <= i < 4: xAngle = 0,      yAngle = 0, zAngle = halfPi
+            * 4 <= i < 6: xAngle = 0,      yAngle = 0, zAngle = 0*/
+            g.rotate((1-i/2+i/4)*halfPi, 0, (i/2-i/4-i/4)*halfPi); // integer division is for comapctness
+            rootNode.attachChild(g);
+        }
+    }
+
+    /**Class that integrates Particles into jME's logic.*/
+    private class Being {
+
+        private final Particle p;
+        private final Geometry g;
+
+        /**Create a Mesh and a Material according to the properties of p,
+         * make a Geometry out of them and attach the Geometry to the rootNode.*/
+        private Being (Particle p) {
+
+            Sphere mesh = new Sphere(30, 30, (float)p.radius); // First 2 arguments control the quality of the sphere.
+            String geometryName = "ball"+BEINGCOUNT;
+            BEINGCOUNT++;
+            Material mat = new Material(assetManager,
+                    "Common/MatDefs/Misc/Unshaded.j3md"); //default material
+            float[] col = p.color();
+            mat.setColor("Color", new ColorRGBA(col[0], col[1], col[2], col[3]));
+
+            Geometry g = new Geometry(geometryName, mesh);
+            g.setMaterial(mat);
+            rootNode.attachChild(g);
+            this.p = p;
+            this.g = g;
+        }
+    }
 }
--- Particle.java
+++ Particle.java
@@ -1,4 +1,4 @@
-import java.awt.Color;
+import edu.princeton.cs.algs4.StdRandom;
 
 /**
  *  A particle moving in a box,
@@ -8,32 +8,34 @@ import java.awt.Color;
  */
 public class Particle {
     private static final double INFINITY = Double.POSITIVE_INFINITY;
-    private static final double BORDERCOORDMAX = 1.0;
-    private static final double BORDERCOORDMIN = 0.0;
-    private static final double VELRANGE = 0.005;
-    private static final double DEFAULTRADIUS = 0.02;
+    private static final double BORDERCOORDMAX = 100.0; //1 for stddraw
+    private static final double BORDERCOORDMIN = -100.0; //0 for stddraw
+    private static final double VELRANGE = 0.5; //0.005 for stddraw
+    private static final double DEFAULTRADIUS = 0.5; //0.02 for stddraw
     private static final double DEFAULTMASS = 0.5;
+    private static final float[] DEFAULTCOLOR = new float[]{0, 1, 0, 1}; //green
 
     public final int DIM; //number of translational degrees fo freedom
     public final double radius;
     public final double mass;
-    public final String color; /*use String to make it compatible both with StdDraw and jme?*/
-    private double[] r; // position
-    private double[] v; // velocity
+    private final float[] color; // array of red, green, blue, alpha values
+    private final double[] r; // position
+    private final double[] v; // velocity
     private int count; // number of collisions so far
 
 
     /**
      * Initializes a particle with the specified position, velocity, radius, mass, and color.
      */
-    public Particle (double[] r, double[] v, double radius, double mass, String color) {
+    public Particle (double[] r, double[] v, double radius, double mass, float[] color) {
         this.r = r.clone(); //defensive copy
         this.v = v.clone(); //defensive copy
         this.DIM = this.r.length;
         assert this.DIM == this.v.length: "Particle creation failure. Position and velocity must have the same number of dimensions.";
         this.radius = radius;
-        this.mass   = mass;
-        this.color  = color;
+        this.mass = mass;
+        this.color = color.clone(); //defensive copy
+        assert this.color.length == 4 : "Particle color has the wrong length.";
     }
 
     /**
@@ -43,7 +45,6 @@ public class Particle {
      * @param  N number of dimensions
      */
     public Particle (int N) {
-
         double[] r = new double[N];
         double[] v = new double[N];
 
@@ -56,7 +57,22 @@ public class Particle {
         this.DIM = N;
         radius = DEFAULTRADIUS;
         mass   = DEFAULTMASS;
-        color  = "BLACK";
+        color  = DEFAULTCOLOR; //black
+    }
+
+    /**Returns (a copy of) this particle's color.*/
+    public float[] color () {
+        return this.color.clone();
+    }
+
+    /**Returns the position in the Nth dimension.*/
+    public double position (int N) {
+        return r[N];
+    }
+
+    /**Returns the velocity in the Nth dimension.*/
+    public double velocity (int N) {
+        return v[N];
     }
 
     /**
@@ -164,6 +180,16 @@ public class Particle {
         }
     }
 
+    /**Calculates timeToHitNWall for all walls.*/
+    public double[] timeToHitWalls () {
+        double[] times = new double[DIM];
+
+        for (int i = 0; i < DIM; i++) {
+            times[i] = timeToHitNWall(i);
+        }
+        return times;
+    }
+
     /**
      * Updates the velocities of this particle and the specified particle according
      * to the laws of elastic collision. Assumes that the particles are colliding
@@ -177,15 +203,15 @@ public class Particle {
         double dvdr = Couve.dotProduct(dv, dr);
         double dist = this.radius + that.radius;   // distance between particle centers at collision
 
-        // impulse from normal forces
+        /*Impulse from normal forces.*/
         double magnitude = 2 * this.mass * that.mass * dvdr / ((this.mass + that.mass) * dist);
         double[] J = Couve.scale(magnitude/dist, dr);
 
-        // update velocities according to the impulse
+        /*Update velocities according to the impulse.*/
         Couve.scaledIncrement(this.v, 1/this.mass, J);
         Couve.scaledIncrement(that.v, -1/that.mass, J);
 
-        // update collision counts
+        /*Update collision counts.*/
         this.count++;
         that.count++;
     }
@@ -198,7 +224,7 @@ public class Particle {
      * @param N the number which identifies an axis that connects two opposite walls
      */
     public void bounceOffNWall (int N) {
-        assert 0<=N && N<v.length: "Invalid axis for wall collision.";
+        assert 0<=N && N<DIM : "Invalid axis for wall collision.";
         v[N] = -v[N];
         count++;
     }
